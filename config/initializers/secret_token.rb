b9607e356a3cbeba0266120d0b800e52a0228b98
%= print(Erin Lee ) <%= print(email.erin@gmail.com )>
2013-03-04 21:47:58 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErinbetaPrelaunch::Application.config.secret_token = '9fabd11dd543dd3211a9fc1d3b7ff66b5b08f2aaa399d88057e6f7fcbdaf6d60c53b73555fce36b5360ba6f48fed363b7532cbeb4318e35546724b02652d91de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ErinbetaPrelaunch::Application.config.session_store :cookie_store, key: '_erinbeta-prelaunch_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ErinbetaPrelaunch::Application.config.session_store :active_record_store
