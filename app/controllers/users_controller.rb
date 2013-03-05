93c4c5357554edd5451a914e6ffd296550bbe134
Erin L <email.erin@gmail.com>
2013-03-04 23:46:43 -0500

    add chart to admin dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,4 +30,16 @@ class UsersController < ApplicationController
       redirect_to users_path, :notice => "Can't delete yourself."
     end
   end
+
+  private
+
+  def create_chart
+    users_by_day = User.group("DATE(created_at)").count
+    data_table = GoogleVisualr::DataTable.new
+    data_table.new_column('date')
+    data_table.new_column('number')
+    users_by_day.each do |day|
+      data_table.add_row([ Date.parse(day[0].to_s), day[1]])
+  end
+  @chart = GoogleVisualr::Interactive::AnnotatedTimeLine.new(data_table)  
 end
